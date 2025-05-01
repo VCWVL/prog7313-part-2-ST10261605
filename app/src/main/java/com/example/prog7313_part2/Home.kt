97156352509b0a721ba0f0e8fee22b3950875990
ST10261605 <ST10261605@vcconnect.edu.za>
2025-05-01 14:25:09 +0200

    Added all screens to nav bar and now allows users to browse through different screens.

--- app/src/main/java/com/example/prog7313_part2/Home.kt
+++ app/src/main/java/com/example/prog7313_part2/Home.kt
@@ -1,14 +1,22 @@
 package com.example.prog7313_part2
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val navView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Connect bottom nav to nav controller
+        navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/example/prog7313_part2/HomeFragment.kt
+++ app/src/main/java/com/example/prog7313_part2/HomeFragment.kt
@@ -11,11 +11,7 @@ import android.view.ViewGroup
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class HomeFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
