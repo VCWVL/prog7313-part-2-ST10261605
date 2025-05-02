8ad0a98de1b3ca18dd8efa7581ea916426c9282b
ST10258543 <st10258543@vcconnect.edu.za>
2025-05-02 10:04:43 +0200

    AddedLoginScreen

--- app/src/main/java/com/example/prog7313_part2/HomeFragment.kt
+++ app/src/main/java/com/example/prog7313_part2/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,6 +32,23 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val addMoney = view.findViewById<Button>(R.id.addMoney)
+        addMoney.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_addMoneyFragment)
+        }
+
+        val addExpense = view.findViewById<Button>(R.id.addExpense)
+        addExpense.setOnClickListener{
+        }
+
+        val transferMoney = view.findViewById<Button>(R.id.transferMoney)
+        transferMoney.setOnClickListener{
+        }
     }
 
     companion object {
