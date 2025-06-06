19b90f9349e0dec4c10bf5db75f3c51401ac0459
ST10261605 <ST10261605@vcconnect.edu.za>
2025-06-06 11:59:40 +0200

    Added navigation to expense and category report pages.

---
+++ app/src/main/java/com/example/prog7313_part2/CategoryReportActivity.kt
@@ -0,0 +1,12 @@
+package com.example.prog7313_part2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CategoryReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_report)
+
+    }
+}
---
+++ app/src/main/java/com/example/prog7313_part2/ExpensesReportActivity.kt
@@ -0,0 +1,12 @@
+package com.example.prog7313_part2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ExpensesReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expenses_report)
+
+    }
+}
