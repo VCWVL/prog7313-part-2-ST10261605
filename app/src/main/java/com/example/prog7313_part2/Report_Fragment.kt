d2e596b9eb4f515ca33b091d38b42bb007cff115
ST10261605 <ST10261605@vcconnect.edu.za>
2025-06-09 15:12:09 +0200

    Fixed expenses report page functionality (attempt 2)

--- app/src/main/java/com/example/prog7313_part2/Report_Fragment.kt
+++ app/src/main/java/com/example/prog7313_part2/Report_Fragment.kt
@@ -8,25 +8,25 @@ import android.view.View
 import android.view.ViewGroup
 
 class Report_Fragment : Fragment() {
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        val view = inflater.inflate(R.layout.fragment_report_, container, false)
-//
-//
-//        val cardViewAllExpenses = view.findViewById<View>(R.id.cardViewAllExpenses)
-//        cardViewAllExpenses.setOnClickListener {
-//            val intent = Intent(requireContext(), ExpensesReportActivity::class.java)
-//            startActivity(intent)
-//        }
-//
-//
-//        val cardViewCategoryAmounts = view.findViewById<View>(R.id.cardViewCategoryAmounts)
-//        cardViewCategoryAmounts.setOnClickListener {
-//            val intent = Intent(requireContext(), CategoryReportActivity::class.java)
-//            startActivity(intent)
-//        }
-//        return view
-//    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_report_, container, false)
+
+
+        val cardViewAllExpenses = view.findViewById<View>(R.id.cardViewAllExpenses)
+        cardViewAllExpenses.setOnClickListener {
+            val intent = Intent(requireContext(), ExpensesReportActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        val cardViewCategoryAmounts = view.findViewById<View>(R.id.cardViewCategoryAmounts)
+        cardViewCategoryAmounts.setOnClickListener {
+            val intent = Intent(requireContext(), CategoryReportActivity::class.java)
+            startActivity(intent)
+        }
+        return view
+    }
 }
